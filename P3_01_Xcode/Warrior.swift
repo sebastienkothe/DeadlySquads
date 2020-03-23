7a1c2819d09abf6d4680fe2f65d091f2f6136693
Sébastien Kothé <sebastien.kothe@icloud.com>
2020-03-23 22:33:55 +0100

    Add func showHealth()

--- P3_01_Xcode/Warrior.swift
+++ P3_01_Xcode/Warrior.swift
@@ -13,6 +13,31 @@ class Warrior {
     var lifePoints: Int = 100
     var attackPoints: Int = 60
     var weapon: Weapon = Bow()
+    var maxHP = 100
+    
+    var isAlive: Bool {
+        lifePoints > 0
+    }
+    
+    var hasMaxHP: Bool {
+        lifePoints >= maxHP
+    }
+    
+    var has0HP: String {
+        return "💔"
+    }
+    
+    var hasLowLife: String {
+        return "❤️"
+    }
+    
+    var hasMediumLife: String {
+        return "❤️❤️"
+    }
+    
+    var hasHighLife: String {
+        return "❤️❤️❤️"
+    }
     
     init(name: String) {
         self.name = name
